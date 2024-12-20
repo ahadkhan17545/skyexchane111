b335a8782a01951b465d2760fc91f3e371e3dac5
md shamsher <mdsamsher530@gmail.com>
2024-12-20 16:01:41 +0530

    done

--- src/pages/LiveMatchs/Baccarat/Baccarat.jsx
+++ src/pages/LiveMatchs/Baccarat/Baccarat.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Menu_select from "../../../components/Menu_select";
 import Bet_slip from "../../../components/Bet_slip";
-// import BaccaratMiddle from "../../../components/LiveBets/BaccaratMiddle";
+import BaccaratMiddle from "../../../components/LiveBets/BaccaratMiddle";
 
 const Baccarat = () => {
   const menuItems = [
